2f597c721f4ef4c55d24b4daccfd286abe6f9785
Katarzyna <kithy@interia.eu>
2019-02-17 14:54:58 +0100

    impl 2

--- src/main/java/account/Logger.java
+++ src/main/java/account/Logger.java
@@ -3,6 +3,9 @@ package account;
 public class Logger {
     private static Logger instance;
 
+    private Logger() {
+    }
+
     public static Logger getInstance() {
         if (instance == null) {
             synchronized (Logger.class) {
